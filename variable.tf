8ec28875479cec4712b1bb4176ac694caaca5f09
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-25 14:02:58 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,11 @@
 variable "aws_access_key" {
-  default = "${var.aws_access_key}"
+  default = "${var.DEV_AWS_ACCESS_KEY_ID}"
   description = "Access key"
   type = string
 }
 
 variable "aws_secret_key" {
-  default = "${var.aws_secret_key}"
+  default = "${var.DEV_AWS_SECRET_ACCESS_KEY}"
   description = "Access secret"
   type = string
 }
