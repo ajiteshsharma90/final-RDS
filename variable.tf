1d9999a7065d0ee114429a242784ebfd1bbdb12a
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-25 16:04:40 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,5 @@
-variable "DEV_AWS_ACCESS_KEY_ID" {
-  default = ${{secrets.DEV_AWS_ACCESS_KEY_ID}}
-  description = "Access key"
-  type = string
-}
-
-variable "DEV_AWS_SECRET_ACCESS_KEY" {
-  default = ${{secrets.DEV_AWS_SECRET_ACCESS_KEY}}
-  description = "Access secret"
-  type = string
-}
-
 variable "aws_region" {
-  default = ${{secret.aws_region}}
+  default = "us-west-2"
   description = "Region"
   type = string
 }
