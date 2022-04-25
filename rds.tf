adb90cc1d66469c78f53987b4f5e4041a5b381bf
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-25 16:04:46 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,3 +1,13 @@
+resource "aws_secretsmanager_secret" "my_password_secret" {
+  name = "/dev/mysql"
+}
+
+data "aws_secretsmanager_secret" "creds" {
+  name = "username"
+  name = "password"
+}
+
+
 resource "aws_db_instance" "default" {
 allocated_storage = 20
 identifier = "testinstance"
@@ -6,7 +16,7 @@ engine = "mysql"
 engine_version = "5.7"
 instance_class = "db.m4.large"
 name = "test"
-username = "aws_username"
-password = "aws_password"
+username = data.aws_secretsmanager_secret.username
+password = data.aws_secretsmanager_secret.password
 parameter_group_name = "default.mysql5.7"
 }
