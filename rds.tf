13ede3ba4c30c6f512ee09cace6c44cd9ef868e6
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-25 16:25:39 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -12,7 +12,7 @@ locals {
 
 resource "aws_db_instance" "default" {
 allocated_storage = 20
-identifier = "testinstance"
+identifier = "testinstance1"
 storage_type = "gp2"
 engine = "mysql"
 engine_version = "5.7"
