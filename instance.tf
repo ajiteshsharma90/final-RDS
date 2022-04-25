bf283c22c63218fa185e149c9ca52a777fe493aa
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-25 16:23:11 +0530

    Update instance.tf
    [CI-SKIP]

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "db-subnet1" {
 name = "db subnet group one"
-subnet_ids = ["${aws_subnet.private-subnet1.id}", "${aws_subnet.private-subnet2.id}"]
+subnet_ids = ["${aws_subnet.private-subnet3.id}", "${aws_subnet.private-subnet4.id}"]
 }
 
