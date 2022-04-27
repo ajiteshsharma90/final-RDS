ef72b76980f72ed109791f6f584081a4771f96a8
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-27 22:11:06 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
-resource "aws_db_subnet_group" "db-subnet1" {
-name = "db subnet group one"
+resource "aws_db_subnet_group" "db-subnet2" {
+name = "db subnet group two"
 subnet_ids = ["${aws_subnet.private-subnet3.id}", "${aws_subnet.private-subnet4.id}"]
 }
 
