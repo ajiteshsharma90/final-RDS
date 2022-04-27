318ecfc31a6baddad3782653dac35c56c33f7b0f
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-27 22:58:07 +0530

    Update rds.tf
    [skip ci]

--- rds.tf
+++ rds.tf
@@ -16,6 +16,7 @@ resource "aws_db_instance" "default" {
   engine               = "sqlserver-se"
   engine_version       = "15.00.4153.1.v1"
   instance_class       = "db.r5.xlarge"
+  license_model             = "license-included"
   #name = "test4"
   username = local.db_creds.username
   password = local.db_creds.password
