d720eeaa992fbf2cc502531dc4d0c76ef5ffb56b
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-27 22:30:32 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -13,10 +13,11 @@ locals {
 resource "aws_db_instance" "default" {
 allocated_storage = 20
 identifier = "testinstance3"
-storage_type = "gp2"
-engine = "sqlserver-ex"
-engine_version = "15.00.4153.1.v1"
-instance_class = "db.r5.large"
+engine               = "sqlserver-ex"
+  engine_version       = "15.00.4153.1.v1"
+  family               = "sqlserver-ex-15.0" # DB parameter group
+  major_engine_version = "15.00"             # DB option group
+  instance_class       = "db.t3.large"
 name = "test4"
 username = local.db_creds.username
 password = local.db_creds.password
