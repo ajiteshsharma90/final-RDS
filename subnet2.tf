e69404881498f0d5a51a8b08cb7b9d20560c53ad
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-28 17:57:27 +0530

    Update subnet2.tf
    skip ci

--- subnet2.tf
+++ subnet2.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private-subnet55" {
 #vpc_id = "${aws_vpc.main.id}"
-vpc_id = "vpc-707fb41b"
+vpc_id = "vpc-00137218a9c55fdaa"
 cidr_block = "10.0.3.0/24"
 availability_zone = "us-west-2b"
 }
