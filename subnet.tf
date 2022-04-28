e2b077c234ce2d1febadc3e00a02cfbb64e9408b
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-28 18:46:35 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private-subnet45" {
 #vpc_id = "${aws_vpc.main.id}"
- vpc_id = var.vpc_id
+ vpc_id = var.VPC_ID
  cidr_block = "10.0.5.0/24"
  availability_zone = "us-west-2a"
 }
