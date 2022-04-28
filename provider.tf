06ccb64479783632aee66cbb2303ed1b565d4b55
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-28 13:30:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,4 +2,6 @@ provider "aws" {
   access_key = var.DEV_AWS_ACCESS_KEY_ID
   secret_key = var.DEV_AWS_SECRET_ACCESS_KEY
   region     = var.AWS_REGION
+  username = var.AWS_USERNAME
+  password = var.AWS_PASSWORD
 }
