92bdd0527062b0ac477652f8b8d44046e68425b3
ajiteshsharma90 <92788280+ajiteshsharma90@users.noreply.github.com>
2022-04-28 16:24:05 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,13 +1,13 @@
-data "aws_secretsmanager_secret_version" "creds" {
+#data "aws_secretsmanager_secret_version" "creds" {
   # Fill in the name you gave to your secret
-  secret_id = "db-creds"
-}
+ # secret_id = "db-creds"
+#}
 
-locals {
-  db_creds = jsondecode(
-    data.aws_secretsmanager_secret_version.creds.secret_string
-  )
-}
+#locals {
+ # db_creds = jsondecode(
+  #  data.aws_secretsmanager_secret_version.creds.secret_string
+  #)
+#}
 
 
 resource "aws_db_instance" "default" {
